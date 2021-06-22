760e5c2a41e8a41aa5c112c90f883cb8d09453cf
Samuel Sacco <SamLSacco@aol.com>
2021-06-22 17:35:21 -0400

    FUCK YA

---
+++ Todos/frontend/reducers/steps_reducer.js
@@ -0,0 +1,35 @@
+import{
+    RECEIVE_STEP,
+    RECEIVE_STEPS,
+    REMOVE_STEP
+} from '../actions/step_actions'
+
+const stepsReducer = (state = {}, action) => {
+    Object.freeze(state);
+    let newState = {};
+
+    switch (action.type) {
+        case RECEIVE_STEPS: // * DAFUQ
+            for (let i = 0; i < action.steps.length; i++) {
+                let step = action.steps[i];
+
+                newState[step.id] = step;
+            }
+
+            return newState;
+        case RECEIVE_STEP:
+            newState = Object.assign({}, state);
+            newState[action.step.id] = action.step;
+
+            return newState;
+        case REMOVE_STEP:
+            newState = Object.assign({}, state);
+            delete newState[action.step.id];
+
+            return newState;
+        default:
+            return state;
+    }
+}
+
+export default stepsReducer;
--- Todos/frontend/todo_redux.jsx
+++ Todos/frontend/todo_redux.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
-// import { receiveTodo } from './actions/todo_actions';
-// import { receiveTodos } from './actions/todo_actions';
-import * as todoActions from './actions/todo_actions';
+import { receiveTodo } from './actions/todo_actions';
+import { receiveTodos } from './actions/todo_actions';
+import { removeTodo } from './actions/todo_actions'
+import { receiveStep } from './actions/step_actions';
+import { receiveSteps } from './actions/step_actions';
+import { removeStep } from './actions/step_actions'
+// import * as todoActions from './actions/todo_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
     // console.log(receiveTodo);
     const store = configureStore();
-    window.todoActions = todoActions;
+    // window.todoActions = todoActions;
     window.store = store;
-    // window.receiveTodo = receiveTodo;
-    // window.receiveTodos = receiveTodos;
+    window.receiveTodo = receiveTodo;
+    window.receiveTodos = receiveTodos;
+    window.removeTodo = removeTodo;
+    window.receiveStep = receiveStep;
+    window.receiveSteps = receiveSteps;
+    window.removeStep = removeStep;
 
     const root = document.getElementById("root");
     ReactDOM.render(<h1>Todos App</h1>, root);
